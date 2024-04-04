8024466b8f1c2fcd26ca627a7cee28ba5c5548cd
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2024-04-04 23:15:05 +0300

    Example Rocket

---
+++ self-affine-tiles/rocket.py
@@ -0,0 +1,23 @@
+from tile import SelfAffineTile
+
+A = [
+    [3, 0],
+    [0, 3]
+]
+
+D = [
+    (0, 0),
+    (1, 1),
+    (2, 2),
+    (-1, 0),
+    (-2, 0),
+    (-1, 1),
+    (0, -1),
+    (0, -2),
+    (1, -1)
+]
+
+x0 = (1/2, 1/2)
+
+Rocket = SelfAffineTile(A, D, x0, "Rocket")
+Rocket(10000, 10)

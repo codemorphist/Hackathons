35eba171931d84a94db650b649413f4a66ec9f5c
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2024-04-21 15:53:08 +0300

    Updated field.py

--- battleship-game/field.py
+++ battleship-game/field.py
@@ -1,17 +1,11 @@
+from typing import List, Dict, Union, TypeAlias
 from coord import Coord
-from ship import Ship, Orientation
-from ship import LinearShip, Frigate, Brig, Gunboat
+from ship import Ship, LinearShip, Frigate, Brig, Gunboat, Orientation
 from mine import Mine
+from exceptions import OutOfField, PlaceError, ShipTooNear, InvalidGameObject, InvalidCount
 
-from typing import List, Dict, Union
-from typing import TypeAlias
-
-from exceptions import *
-
-
-GameObject: TypeAlias = Union[Ship, Mine]
-GameObjects: TypeAlias = List[GameObject]
-
+GameObject = Union[Ship, Mine]
+GameObjects = List[GameObject]
 
 DefaultRules = {
     LinearShip: 1,
@@ -21,25 +15,18 @@ DefaultRules = {
     Mine: 2
 }
 
-
 class Attacked:
-    pass
-
+    def __repr__(self) -> str:
+        return "Attacked()"
 
 class Field:
     def __init__(self, 
-                 objects: GameObject = [], 
-                 size: int = 8,
+                 objects: GameObjects = [], 
+                 size: int = 8, 
                  rules: Dict[type, int] = DefaultRules):
-        """
-        :param objects: objects placed on Field
-        :param size: size of field
-        :param rules: rules for cout of ships
-        """
-        self._map = {} 
+        self._map = {}
         self._size = size
         self._rules = rules
-
         self.place_objects(objects)
 
     def place_objects(self, objects: GameObjects):
@@ -47,34 +34,35 @@ class Field:
             self.place_object(obj)
 
     def place_object(self, obj: GameObject):
-        """
-        Place object on field by them coords
-
-        :param obj: object to place
-        """
         if isinstance(obj, Mine):
-            if not self.on_field(obj.pos):
-                raise OutOfField(obj)
-            if obj.pos in self._map:
-                raise PlaceError(obj, self.get_object(obj.pos))
-
-            self._map[obj.pos] = (obj, None)
-            self._change_rule(obj)
+            self._place_mine(obj)
         elif isinstance(obj, Ship):
-            curr = obj.pos
-            for deck in range(obj.size):
-                if not self.on_field(curr):
-                    raise OutOfField(obj)
-                if curr in self._map:
-                    raise PlaceError(obj, self.get_object(curr))
-                if self._ship_near(obj, curr):
-                    raise ShipTooNear(obj)
-
-                self._map[curr] = (obj, deck)
-                curr += obj.orient.value
-            self._change_rule(obj)
+            self._place_ship(obj)
         else:
             raise InvalidGameObject(obj)
+
+    def _place_mine(self, obj: Mine):
+        if not self.on_field(obj.pos):
+            raise OutOfField(obj)
+        if obj.pos in self._map:
+            raise PlaceError(obj, self.get_object(obj.pos))
+
+        self._map[obj.pos] = (obj, None)
+        self._change_rule(obj)
+
+    def _place_ship(self, obj: Ship):
+        curr = obj.pos
+        for deck in range(obj.size):
+            if not self.on_field(curr):
+                raise OutOfField(obj)
+            if curr in self._map:
+                raise PlaceError(obj, self.get_object(curr))
+            if self._ship_near(obj, curr):
+                raise ShipTooNear(obj)
+            self._map[curr] = (obj, deck)
+
+            curr += obj.orient.value
+        self._change_rule(obj)
 
     def _ship_near(self, ship: Ship, coord: Coord) -> bool:
         for x in range(coord.x - 1, coord.x + 2):
@@ -93,7 +81,7 @@ class Field:
         t = type(obj)
         if t not in self._rules:
             raise InvalidGameObject(obj)
-        if self._rules[t] <= 0:
+        elif self._rules[t] <= 0:
             raise InvalidCount(t)
         else:
             self._rules[t] -= 1
@@ -107,77 +95,96 @@ class Field:
         return self._size
 
     def get_object(self, coord: Coord) -> GameObject:
-        if coord not in self._map:
-            return None, None
-        else:
-            return self._map[coord]
+        return self._map.get(coord, (None, None))
 
     def on_field(self, coord: Coord) -> bool:
         x, y = coord
         return 0 <= x < self.size and 0 <= y < self.size
 
-    def attack(self, coord: Coord) -> Union[Mine, None]:
-        obj, info = self.get_object(coord)
-        if obj is None:
+    def attack(self, coord: Coord) -> Union[Ship, Mine, None]:
+        obj, i = self.get_object(coord)
+        if obj is None or isinstance(obj, Mine):
             self._map[coord] = (Attacked(), None)
         elif isinstance(obj, Ship):
-            obj.attack_deck(info)
-
+            obj.attack_deck(i)
             if obj.is_destroyed():
-                start = obj.pos + Coord(-1, 1)
-                end = obj.pos + obj.size * obj.orient.value + Coord(1, 0)
+                start, end = obj.get_start_end()
 
-                for c in self._coord_generator(start, end):
-                    if not self.on_field(c):
+                for coord in self.iterate_rectangle(start, end):
+                    obj, _ = self.get_object(coord)
+                    if isinstance(obj, Ship):
                         continue
-                    if c in self._map and self.get_object(c):
-                        continue
-                    self._map[c] = (Attacked(), None)
-        else:
-            del self._map[coord]
-            return obj
-        
-    def _coord_generator(self, start: Coord, end: Coord):
-        sx, sy = start 
-        ex, ey = end
-        for y in range(sy, ey-1, -1):
-            for x in range(sx, ex+1):
-                print(x, y)
-                yield Coord(x, y)
+                    self._map[coord] = (Attacked(), None)
+        return obj
+    
+    @property
+    def field(self):
+        field = {}
+        for c, obj in self.map.items():
+            if isinstance(obj[0], Attacked):
+                field[c] = obj
+            if isinstance(obj[0], Ship) and obj[0].is_destroyed():
+                field[c] = obj
+        return field
+
+    def iterate_rectangle(self, start: Coord, end: Coord):
+        x1, y1 = start
+        x2, y2 = end
+
+        min_x = min(x1, x2)
+        max_x = max(x1, x2)
+        min_y = min(y1, y2)
+        max_y = max(y1, y2)
+    
+        for x in range(min_x, max_x + 1):
+            for y in range(min_y, max_y + 1):
+                if self.on_field(Coord(x, y)):
+                    yield Coord(x, y)
+
 
 if __name__ == "__main__":
     f = Field(
         [
             LinearShip(Coord(0, 0), Orientation.Down),
             Frigate(Coord(0, 5), Orientation.Down),
-            Frigate(Coord(2, 0), Orientation.Right),
+            Frigate(Coord(4, 1), Orientation.Left),
             Brig(Coord(6, 0), Orientation.Right),
             Brig(Coord(2, 7), Orientation.Right),
             Brig(Coord(5, 6), Orientation.Up),
             Gunboat(Coord(3, 3), Orientation.Up),
-            Gunboat(Coord(2, 5), Orientation.Up),
+            Gunboat(Coord(2, 5), Orientation.Left),
+            Mine(Coord(4, 4), 8)
         ]
     )
 
-    f.attack(Coord(5, 6))
-    f.attack(Coord(5, 5))
+    CYAN = "\u001b[36m"
+    RED = "\u001b[31m"
+    GREEN = "\u001b[32m"
+    MAGENTA = "\u001b[35m"
+    BLUE = "\u001b[34m"
+    RESET = "\u001b[0m"
 
-    # f.attack(Coord(2, 5))
 
-    print(" "," ".join([chr(ord("a")+i) for i in range(f.size)]))
+    f.attack(Coord(2, 5))
+    f.attack(Coord(2, 1))
+    f.attack(Coord(3, 1))
+    f.attack(Coord(4, 1))
+
+
+    print(" ", BLUE + " ".join([chr(ord("a")+i) for i in range(f.size)]) + RESET)
     r = 1
     for y in range(f.size):
-        print(r, end=" ")
+        print(BLUE + str(r) + RESET, end=" ")
         for x in range(f.size):
             curr = Coord(x, y)
             obj, i = f.get_object(curr)
             if isinstance(obj, Mine):
-                print("o", end=" ")
+                print(MAGENTA + "o" + RESET, end=" ")
             elif isinstance(obj, Ship):
-                print("x" if obj.decks[i] else "#", end=" ")
+                print(((RED + "x") if obj.decks[i] else (RESET + "#")) + RESET, end=" ")
             elif isinstance(obj, Attacked):
-                print(".", end=" ") 
+                print(GREEN + "." + RESET, end=" ") 
             else:
-                print("~", end=" ")
+                print(CYAN + "~" + RESET, end=" ")
         print()
         r += 1
